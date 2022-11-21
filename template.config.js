228038321e20cce82ab1ae0e1d38934b58f911c3
Noel Miranda <55200516+NoelMiranda1@users.noreply.github.com>
2022-11-21 14:35:52 -0600

    Initial commit

---
+++ template.config.js
@@ -0,0 +1,13 @@
+/** @format */
+
+module.exports = {
+  // Placeholder used to rename and replace in files
+  // package.json, index.json, android/, ios/
+  placeholderName: "template",
+
+  // Directory with template
+  templateDir: "./template",
+
+  // Path to script, which will be executed after init
+  postInitScript: "./script.js"
+};
